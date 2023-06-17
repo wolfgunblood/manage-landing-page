4181a21d0ac75bfdbe51622248da8243daec1230
wolfgunblood <jalajdorai1@gmail.com>
2023-06-17 08:45:53 +0530

    Updated Hero Section

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,17 +1,20 @@
-import React from 'react'
-import GetStarted from '../GetStarted/GetStarted'
+import React from 'react';
+import "./Hero.scss";
+import GetStarted from '../GetStarted/GetStarted';
+import Intro from "../../assets/images/illustration-intro.svg";
+// import Bg from "../../assets/images/bg-simplify-section-desktop.svg";
 
 const Hero = () => {
     return (
         <div className='hero-container'>
             <div className='hero-content'>
-                <h1> Bring everyone together to build better products.</h1>
-                <p> Manage makes it simple for software teams to plan day-to-day
+                <h1 className='hero-content-title'> Bring everyone together to build better products.</h1>
+                <p className='hero-content-description'> Manage makes it simple for software teams to plan day-to-day
                     tasks while keeping the larger team goals in view.</p>
                 <GetStarted />
             </div>
             <div className='hero-image'>
-
+                <img src={Intro} alt="Ilustration Intro" />
             </div>
         </div>
     )
