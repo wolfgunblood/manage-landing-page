477f38552edf90c093e213cc99ce565470fed8df
wolfgunblood <jalajdorai1@gmail.com>
2023-06-17 08:12:42 +0530

    Updated Hero and Created Get Started Button

---
+++ src/components/Hero/Hero.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Hero = () => {
+  return (
+    <div className='hero-container'>
+        <div className='hero-content'>
+            <h1> Bring everyone together to build better products.</h1>
+            <p> Manage makes it simple for software teams to plan day-to-day 
+  tasks while keeping the larger team goals in view.</p>
+        </div>
+        <div className='hero-image'>
+
+        </div>
+    </div>
+  )
+}
+
+export default Hero
